ad1f83c91bb2652661768f7dbd6b5d9a360de411
EJUNBIN <junbin.zhong@ericsson.com>
2017-01-10 10:56:05 +0800

    port from env

--- main.go
+++ main.go
@@ -4,15 +4,11 @@ import (
 	"log"
 	"net/http"
 	"github.com/elazarl/goproxy"
-	"fmt"
-	"os"
 )
 
 func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	log.Fatal(http.ListenAndServe(addr, proxy))
+	log.Fatal(http.ListenAndServe(":80", proxy))
 
 }
